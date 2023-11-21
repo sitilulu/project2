89f3c6acaa053b54c605e5f600392ba97f90b043
malikazh12 <malikazhar4170@gmail.com>
2023-11-21 15:56:34 +0700

    mml

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/project-new/project2/';
+$config['base_url'] = 'http://localhost/git/project2/';
 
 /*
 |--------------------------------------------------------------------------
@@ -530,3 +530,4 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+
--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,4 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+<<<<<<< HEAD
 class Login extends CI_Controller {} 
+=======
+class Login extends CI_Controller {
+    public function index(){
+        $this->load->view('login');
+    }
+}
+>>>>>>> e4dad5b8c8def7ec57b3304bf93d47b24e0ae17b
